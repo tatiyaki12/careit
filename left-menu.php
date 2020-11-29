e18453a70fbcff457fc16f0ccf98507126be39f1
Richard Illescas <richardillescas2@gmail.com>
2020-11-30 00:00:43 +0800

    First commit

---
+++ left-menu.php
@@ -0,0 +1,41 @@
+<!-- BEGIN: Main Menu-->
+    <div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
+        <div class="navbar-header">
+            <ul class="nav navbar-nav flex-row">
+                <li class="nav-item mr-auto"><a class="navbar-brand" href="#">
+                        <div class="brand-logo"></div>
+                        <h2 class="brand-text mb-0">Vuexy</h2>
+                    </a></li>
+                <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="icon-disc"></i></a></li>
+            </ul>
+        </div>
+        <div class="shadow-bottom"></div>
+        <div class="main-menu-content">
+            <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+                <li class=" nav-item active"><a href="index.php"><i class="feather icon-home"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span><span class="badge badge badge-warning badge-pill float-right mr-2">2</span></a>
+                </li>                
+                <li class=" nav-item"><a href="appointment-list.php"><i class="feather icon-users"></i><span class="menu-title" data-i18n="Appointment">Appointment</span><span class="badge badge badge-warning badge-pill float-right mr-2">2</span></a>
+                </li>
+                <li class=" nav-item"><a href="#"><i class="feather icon-file"></i><span class="menu-title" data-i18n="Room bookings">Room bookings</span></a>
+                </li>
+                <li class=" nav-item"><a href="#"><i class="feather icon-user"></i><span class="menu-title" data-i18n="Profiles">Profiles</span></a>
+                </li>
+                <li class=" nav-item"><a href="#"><i class="feather icon-file"></i><span class="menu-title" data-i18n="Invoice">Invoice</span></a>
+                </li>
+                <li class=" navigation-header"><span>Settings</span>
+                </li>
+                <li class=" nav-item"><a href="#"><i class="feather icon-users"></i><span class="menu-title" data-i18n="Users">Users</span></a>
+                </li>
+                <li class=" nav-item"><a href="#"><i class="feather icon-message-square"></i><span class="menu-title" data-i18n="Floors">Floors</span></a>
+                </li>
+                <li class=" nav-item"><a href="#"><i class="feather icon-check-square"></i><span class="menu-title" data-i18n="Rooms">Rooms</span></a>
+                </li>
+                <li class=" nav-item"><a href="#"><i class="feather icon-calendar"></i><span class="menu-title" data-i18n="Room Category">Room Categories</span></a>
+                </li>
+                <li class=" nav-item"><a href="#"><i class="feather icon-calendar"></i><span class="menu-title" data-i18n="Beds">Beds</span></a>
+                </li>              
+               
+            </ul>
+        </div>
+    </div>
+    <!-- END: Main Menu-->
